85679d6925466944d96bd5499bd742f59847cca3
Stefan Nozinic <ra125-2016@s216.(none)>
2016-10-14 10:15:44 +0200

    zadatak

--- v02/z15.c
+++ v02/z15.c
@@ -3,10 +3,18 @@
 
 int main() 
 {
-	int a,b; 
-	printf("Unesite dva broja: ");
-	scanf(" %d %d", &a, &b);
-	int c = (a<b) ? a : b;
-	printf("Manja vrednost je %d\n", c);
+	int n; 
+	float a[20];
+	printf("Unesi n: ");
+	scanf(" %d", &n);
+	printf("Unesi elemente niza: ");
+	int i; 
+	for (i=0; i<n; i++) scanf(" %f", &a[i]);
+	float max = a[0]; 
+	for (i=0; i<n; i++) 
+	{
+		if (a[i] > max) max = a[i];
+	}
+	printf("Maksimalan element je %f\n", max);
 	return 0;
 }
