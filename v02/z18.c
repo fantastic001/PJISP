3fe5dc2d1c0c1f1cbea608a851e8b8359f53bc47
Stefan Nozinic <ra125-2016@s209.(none)>
2016-10-21 07:16:34 +0200

    zadatak 18

--- v02/z18.c
+++ v02/z18.c
@@ -1,12 +1,33 @@
 
 #include <stdio.h>
 
+int abs(int a) 
+{
+	if (a<0) return -a;
+	return a;
+}
+
 int main() 
 {
-	int a,b; 
-	printf("Unesite dva broja: ");
-	scanf(" %d %d", &a, &b);
-	int c = (a<b) ? a : b;
-	printf("Manja vrednost je %d\n", c);
+	int a[20];
+	int n;
+	int s = 0; 
+	scanf(" %d", &n);
+	int i;
+	for (i=0; i<n; i++) scanf(" %d", &a[i]);
+
+	for (i=0; i<n; i++) s += a[i];
+	s /= n;
+	int min = a[0];
+	int index = 0;
+	for (i=0; i<n; i++) 
+	{
+		if (abs(a[i] - s) < min) 
+		{
+			index = i; 
+			min = abs(a[i] - s);
+		}
+	}
+	printf("%d\n", a[index]);
 	return 0;
 }
